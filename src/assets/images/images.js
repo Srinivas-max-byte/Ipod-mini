357fe5cce46a3a6c9573572831a654004dc8622a
Srinivas <msrinivas0025@gmail.com>
2023-06-09 11:59:20 +0530

    First Commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import wallpaper1 from "./wallpaper1.jpg";
+import wallpaper2 from "./wallpaper2.jpg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import coverflow from "./coverflow.png";
+import games from "./games.jpeg";
+import allsongs from "./allsongs.png";
+import artists from "./artists.jpeg";
+import albums from "./albums.jpeg";
+import song1Img from "./maan meri jaan.jpg";
+import song2Img from "./kesariya.jpg";
+import song3Img from "./ram siya ram.jpg";
+
+const images = {
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./Maan Meri Jaan.mp3";
+import music2 from "./Kesariya.mp3";
+import music3 from "./Ram Siya Ram.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
